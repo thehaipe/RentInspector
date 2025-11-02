1ba360cbb738dcb2eb9c19e92de64d318e5f88d8
thehaipe <thehaipe2007@gmail.com>
2025-11-02 15:12:42 +0200

    Initial Commit

---
+++ RentInspector/RentInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentInspectorApp.swift
+//  RentInspector
+//
+//  Created by Valentyn on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RentInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
